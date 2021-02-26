f332593af609d9a73a19ff8a1f03e34c0421bd78
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-26 01:23:14 -0500

    working on potion function and testing

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,6 +1,9 @@
-test('creates a health potion object', () => {
-  const potion = new potion('health');
+const Potion = require('../lib/Potion')
 
-  expect(potion.name).toBe('health');
+test('creates a health potion object', () => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
   expect(potion.value).toEqual(expect.any(Number));
 });
