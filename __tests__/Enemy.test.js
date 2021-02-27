d5569370af70413c0cb6bf74540daa0578f377b5
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-27 11:23:14 -0500

    enemy and player functions

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -14,3 +14,39 @@ test('creates an enemy object', () => {
   expect(enemy.agility).toEqual(expect.any(Number));
   expect(enemy.potion).toEqual(expect.any(Object));
 })
+
+test('get enemy health value', () => {
+  const enemy = new Enemy('goblin', 'sword');
+
+  expect(enemy.isAlive()).toBeTruthy();
+
+  enemy.health = 0;
+
+  expect(enemy.isAlive()).toBeFalsy();
+});
+
+test('get enemy attack value', () => {
+  const enemy = new Enemy('goblin', 'sword');
+  enemy.strength = 10;
+
+  expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('subtract from enemy health', () => {
+  const enemy = new Enemy('goblin', 'sword');
+  const oldHealth = enemy.health;
+
+  enemy.reduceHealth(5);
+  expect(enemy.health).toBe(oldHealth - 5);
+
+  enemy.reduceHealth(9999);
+  expect(enemy.health).toBe(0);
+});
+
+test('describe enemy weapon', () => {
+  const enemy = new Enemy('goblin', 'sword');
+
+  expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+  expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+})
