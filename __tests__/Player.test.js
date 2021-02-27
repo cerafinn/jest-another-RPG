a1ead8fbaad5623a206ca382b466ba28ba756261
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-27 00:16:19 -0500

    created methods for an object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,8 +3,6 @@ const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
-console.log(new Potion())
-
 test('creates a player object', () => {
   const player = new Player('Dave');
 
@@ -14,4 +12,19 @@ test('creates a player object', () => {
   expect(player.agility).toEqual(expect.any(Number));
 
   expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('get inventory from player or return false', () => {
+  const player = new Player('Dave');
+  
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 })
